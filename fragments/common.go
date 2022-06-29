67fb2f9de6b0402a63ec3795fcdae17ed5595ef5
Colley <rachael.colley@accenture.com>
2022-06-29 14:36:20 +1000

    remove empty lines from flexipage assignment

--- fragments/common.go
+++ fragments/common.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -72,4 +73,90 @@ func saveData(dataString string) {
 	}
 
 	fmt.Println("Fragment created!")
+
+	//removeEmptyLines()
 }
+
+func removeEmptyLines() {
+	fmt.Println("REMOVE EMPTY LINES!")
+	f := openFile()
+	defer f.Close()
+
+	//newLines := []string{}
+	lines := readMergeValues("fragments/output/output-data.txt")
+	for _, line := range lines {
+
+		// str := strings.TrimSpace(line)
+		lineLen := len(line)
+		fmt.Println(line + " " + strconv.Itoa(lineLen))
+		if len(line) > 1 {
+			//newLines = append(newLines, line)
+			if _, err := fmt.Fprintf(f, "%s\n", line); err != nil {
+				log.Fatal(err)
+			}
+		}
+		//s = strings.Replace(lines, "\n\n", "\n", 1)
+	}
+
+}
+
+// func removeEmptyLines() {
+// 	fmt.Println("REMOVE EMPTY LINES!")
+// 	f := openFile()
+// 	defer f.Close()
+
+// 	//newLines := []string{}
+// 	lines := readMergeValues("fragments/output/output-data.txt")
+// 	for _, line := range lines {
+
+// 		str := strings.TrimSpace(line)
+// 		fmt.Println(len(str))
+// 		if len(str) > 0 {
+// 			//newLines = append(newLines, line)
+// 			if _, err := fmt.Fprintf(f, "%s\n", str); err != nil {
+// 				log.Fatal(err)
+// 			}
+// 		}
+// 		//s = strings.Replace(lines, "\n\n", "\n", 1)
+// 	}
+
+// }
+
+// func removeEmptyLines() {
+// 	fmt.Println("REMOVE EMPTY LINES!")
+// 	f := openFile()
+// 	defer f.Close()
+
+// 	//newLines := []string{}
+// 	lines := readMergeValues("fragments/output/output-data.txt")
+// 	for _, line := range lines {
+// 		if len(line) > 0 {
+// 			str := strings.TrimSpace(line)
+// 			fmt.Println(len(str))
+// 			//newLines = append(newLines, line)
+// 			if _, err := fmt.Fprintf(f, "%s\n", str); err != nil {
+// 				log.Fatal(err)
+// 			}
+// 		}
+// 		//s = strings.Replace(lines, "\n\n", "\n", 1)
+// 	}
+
+// }
+
+// func removeEmptyLines() {
+// 	newLines := []string{}
+// 	lines := readMergeValues("fragments/output/output-data.txt")
+// 	for _, line := range lines {
+// 		if len(line) > 0 {
+// 			newLines = append(newLines, line)
+// 		}
+// 		//s = strings.Replace(lines, "\n\n", "\n", 1)
+// 	}
+
+// 	f := openFile()
+// 	defer f.Close()
+
+// 	if _, err := fmt.Fprintf(f, "%s\n", merge4); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
--- fragments/flexipage-assignment.go
+++ fragments/flexipage-assignment.go
@@ -13,11 +13,12 @@ type flexipageAttributes struct {
 	profileName    string
 }
 
-func FlexipageAssignments(hasRecordType bool) {
+func FlexipageAssignments() {
 
-	if hasRecordType {
-
-	}
+	// newplaceholderNames := []string{}
+	// if hasRecordType {
+	// 	newplaceholderNames := []string{"FLEXIPAGENAME", "OBJECTAPINAME", "RECORDTYPENAME", "PROFILENAME"}
+	// }
 
 	mergeValues := getFlexipageMergeValues("fragments/merge-data/flexipage-assignment.txt")
 	template := readTemplate("fragments/templates/flexipage-assignment-template.txt")
@@ -29,13 +30,25 @@ func makeFlexipageAssignmentFragments(template string, placeholderNames []string
 	f := openFile()
 	defer f.Close()
 
+	rtElementText := "    <recordType>RECORDTYPENAME</recordType>"
+
 	start := 0
 	stop := len(mergeValues) - 1
 	for i := start; i <= stop; i++ {
 		n := mergeValues[i]
 		merge1 := merge(template, placeholderNames[0], n.flexipageName)
 		merge2 := merge(merge1, placeholderNames[1], n.objectApiName)
-		merge3 := merge(merge2, placeholderNames[2], n.recordtypename)
+
+		mergeText := n.recordtypename
+		placeholderfragment := placeholderNames[2]
+		if n.recordtypename == "NONE" {
+			mergeText = ""
+			placeholderfragment = rtElementText
+		}
+
+		// merge3 := merge(merge2, placeholderNames[2], n.recordtypename)
+		merge3 := merge(merge2, placeholderfragment, mergeText)
+
 		merge4 := merge(merge3, placeholderNames[3], n.profileName)
 
 		// if _, err := fmt.Fprintf(f, "%s\n%s\n", dateTimeString(), merge4); err != nil {
@@ -45,9 +58,37 @@ func makeFlexipageAssignmentFragments(template string, placeholderNames []string
 		if _, err := fmt.Fprintf(f, "%s\n", merge4); err != nil {
 			log.Fatal(err)
 		}
+
 	}
+	removeEmptyLines()
 	fmt.Println("Fragment created!")
 }
+
+// func makeFlexipageAssignmentFragments(template string, placeholderNames []string, mergeValues []flexipageAttributes) {
+// 	f := openFile()
+// 	defer f.Close()
+
+// 	rtElementText := "    <recordType>RECORDTYPENAME</recordType>"
+
+// 	start := 0
+// 	stop := len(mergeValues) - 1
+// 	for i := start; i <= stop; i++ {
+// 		n := mergeValues[i]
+// 		merge1 := merge(template, placeholderNames[0], n.flexipageName)
+// 		merge2 := merge(merge1, placeholderNames[1], n.objectApiName)
+// 		merge3 := merge(merge2, placeholderNames[2], n.recordtypename)
+// 		merge4 := merge(merge3, placeholderNames[3], n.profileName)
+
+// 		// if _, err := fmt.Fprintf(f, "%s\n%s\n", dateTimeString(), merge4); err != nil {
+// 		// 	log.Fatal(err)
+// 		// }
+
+// 		if _, err := fmt.Fprintf(f, "%s\n", merge4); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+// 	fmt.Println("Fragment created!")
+// }
 
 func getFlexipageMergeValues(mergeValuesFile string) []flexipageAttributes {
 	mergeValueLines := readMergeValues(mergeValuesFile)
